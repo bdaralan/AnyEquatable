8c08bb30979f0e6bf5467ee4056d578ff7bcd6a9
bdaralan <bdaralan@gmail.com>
2024-07-27 11:03:19 -0700

    Initial Commit

---
+++ Package.swift
@@ -0,0 +1,23 @@
+// swift-tools-version: 5.10
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "AnyEquatable",
+    products: [
+        // Products define the executables and libraries a package produces, making them visible to other packages.
+        .library(
+            name: "AnyEquatable",
+            targets: ["AnyEquatable"]),
+    ],
+    targets: [
+        // Targets are the basic building blocks of a package, defining a module or a test suite.
+        // Targets can depend on other targets in this package and products from dependencies.
+        .target(
+            name: "AnyEquatable"),
+        .testTarget(
+            name: "AnyEquatableTests",
+            dependencies: ["AnyEquatable"]),
+    ]
+)
